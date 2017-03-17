e4f38f45f6f11286d48cb680ddba6dbcf2273a40
Parker Lusk <parkerclusk@gmail.com>
2017-03-17 15:36:23 -0600

    beecorelite blinks

--- src/main/drivers/gpio.h
+++ src/main/drivers/gpio.h
@@ -19,21 +19,6 @@
 
 #include "platform.h"
 
-#ifdef STM32F1
-typedef enum
-{
-    Mode_AIN = 0x0,
-    Mode_IN_FLOATING = 0x04,
-    Mode_IPD = 0x28,
-    Mode_IPU = 0x48,
-    Mode_Out_OD = 0x14,
-    Mode_Out_PP = 0x10,
-    Mode_AF_OD = 0x1C,
-    Mode_AF_PP = 0x18
-} GPIO_Mode;
-#endif
-
-#ifdef STM32F3
 typedef enum
 {
     Mode_AIN =          (GPIO_PuPd_NOPULL << 2) | GPIO_Mode_AN,
@@ -47,39 +32,6 @@ typedef enum
     Mode_AF_PP_PD =     (GPIO_OType_PP << 4) | (GPIO_PuPd_DOWN  << 2) | GPIO_Mode_AF,
     Mode_AF_PP_PU =     (GPIO_OType_PP << 4) | (GPIO_PuPd_UP    << 2) | GPIO_Mode_AF
 } GPIO_Mode;
-#endif
-
-#ifdef STM32F4
-typedef enum
-{
-    Mode_AIN         = (GPIO_PuPd_NOPULL << 2) | GPIO_Mode_AN,
-    Mode_IN_FLOATING = (GPIO_PuPd_NOPULL << 2) | GPIO_Mode_IN,
-    Mode_IPD         = (GPIO_PuPd_DOWN   << 2) | GPIO_Mode_IN,
-    Mode_IPU         = (GPIO_PuPd_UP     << 2) | GPIO_Mode_IN,
-    Mode_Out_OD      = (GPIO_OType_OD    << 4) | GPIO_Mode_OUT,
-    Mode_Out_PP      = (GPIO_OType_PP    << 4) | GPIO_Mode_OUT,
-    Mode_AF_OD       = (GPIO_OType_OD    << 4) | GPIO_Mode_AF,
-    Mode_AF_PP       = (GPIO_OType_PP    << 4) | GPIO_Mode_AF,
-    Mode_AF_PP_PD    = (GPIO_OType_PP    << 4) | (GPIO_PuPd_DOWN  << 2) | GPIO_Mode_AF,
-    Mode_AF_PP_PU    = (GPIO_OType_PP    << 4) | (GPIO_PuPd_UP    << 2) | GPIO_Mode_AF
-} GPIO_Mode;
-#endif
-
-#ifdef STM32F7
-typedef enum
-{
-    Mode_AIN         = (GPIO_NOPULL << 5) | GPIO_MODE_ANALOG,
-    Mode_IN_FLOATING = (GPIO_NOPULL << 5) | GPIO_MODE_INPUT,
-    Mode_IPD         = (GPIO_PULLDOWN << 5) | GPIO_MODE_INPUT,
-    Mode_IPU         = (GPIO_PULLUP   << 5) | GPIO_MODE_INPUT,
-    Mode_Out_OD      = GPIO_MODE_OUTPUT_OD,
-    Mode_Out_PP      = GPIO_MODE_OUTPUT_PP,
-    Mode_AF_OD       = GPIO_MODE_AF_OD,
-    Mode_AF_PP       = GPIO_MODE_AF_PP,
-    Mode_AF_PP_PD    = (GPIO_PULLDOWN << 5) | GPIO_MODE_AF_PP,
-    Mode_AF_PP_PU    = (GPIO_PULLUP   << 5) | GPIO_MODE_AF_PP
-} GPIO_Mode;
-#endif
 
 typedef enum
 {
@@ -116,25 +68,8 @@ typedef struct
     GPIO_Speed speed;
 } gpio_config_t;
 
-#ifndef UNIT_TEST
-#if defined(USE_HAL_DRIVER)
-static inline void digitalHi(GPIO_TypeDef *p, uint16_t i) { HAL_GPIO_WritePin(p,i,GPIO_PIN_SET); }
-static inline void digitalLo(GPIO_TypeDef *p, uint16_t i) { HAL_GPIO_WritePin(p,i,GPIO_PIN_RESET); }
-static inline void digitalToggle(GPIO_TypeDef *p, uint16_t i) { HAL_GPIO_TogglePin(p,i); }
-static inline uint16_t digitalIn(GPIO_TypeDef *p, uint16_t i) { return HAL_GPIO_ReadPin(p,i); }
-#else
-#if defined(STM32F4)
-static inline void digitalHi(GPIO_TypeDef *p, uint16_t i) { p->BSRRL = i; }
-static inline void digitalLo(GPIO_TypeDef *p, uint16_t i) { p->BSRRH = i; }
-#else
 static inline void digitalHi(GPIO_TypeDef *p, uint16_t i) { p->BSRR = i; }
 static inline void digitalLo(GPIO_TypeDef *p, uint16_t i) { p->BRR = i; }
-#endif
-static inline void digitalToggle(GPIO_TypeDef *p, uint16_t i) { p->ODR ^= i; }
-static inline uint16_t digitalIn(GPIO_TypeDef *p, uint16_t i) { return p->IDR & i; }
-#endif
-#endif
-
 
 void gpioInit(GPIO_TypeDef *gpio, gpio_config_t *config);
 void gpioExtiLineConfig(uint8_t portsrc, uint8_t pinsrc);
--- src/main/target/BEECORELITE/mainf3.c
+++ src/main/target/BEECORELITE/mainf3.c
@@ -17,16 +17,146 @@
 
 #include <stdbool.h>
 #include <stdint.h>
+#include <string.h>
 
 #include "platform.h"
 
-#include "fc/fc_init.h"
+// #include "common/axis.h"
+// #include "common/color.h"
+// #include "common/maths.h"
+#include "common/printf.h"
 
-#include "scheduler/scheduler.h"
+#include "config/config_eeprom.h"
+#include "config/config_profile.h"
+#include "config/feature.h"
+// #include "config/parameter_group.h"
+#include "config/parameter_group_ids.h"
 
-int main(void)
-{
-    init();
+// #include "drivers/nvic.h"
+// #include "drivers/sensor.h"
+#include "drivers/system.h"
+// #include "drivers/dma.h"
+#include "drivers/io.h"
+// #include "drivers/light_led.h"
+#include "drivers/timer.h"
+#include "drivers/serial.h"
+#include "drivers/serial_uart.h"
+#include "drivers/gpio.h"
+// #include "drivers/accgyro.h"
+// #include "drivers/pwm_esc_detect.h"
+// #include "drivers/rx_pwm.h"
+// #include "drivers/pwm_output.h"
+// #include "drivers/adc.h"
+// #include "drivers/bus_i2c.h"
+#include "drivers/bus_spi.h"
+// #include "drivers/inverter.h"
+// #include "drivers/usb_io.h"
+// #include "drivers/exti.h"
+
+// #include "fc/config.h"
+// #include "fc/fc_init.h"
+// #include "fc/fc_msp.h"
+// #include "fc/fc_tasks.h"
+// #include "fc/rc_controls.h"
+// #include "fc/runtime_config.h"
+// #include "fc/cli.h"
+
+// #include "msp/msp_serial.h"
+
+// #include "rx/rx.h"
+// #include "rx/spektrum.h"
+
+// #include "io/serial.h"
+// #include "io/motors.h"
+
+// #include "scheduler/scheduler.h"
+
+// #include "sensors/acceleration.h"
+// #include "sensors/battery.h"
+// #include "sensors/boardalignment.h"
+// #include "sensors/gyro.h"
+// #include "sensors/initialisation.h"
+// #include "sensors/sensors.h"
+
+// #include "flight/failsafe.h"
+// #include "flight/imu.h"
+// #include "flight/mixer.h"
+// #include "flight/pid.h"
+
+#include "cereal/cereal.h"
+
+// #include "build/build_config.h"
+// #include "build/debug.h"
+
+// ----------------------------------------------------------------------------
+// ----------------------------------------------------------------------------
+
+void LEDInit() {
+    gpio_config_t cfg;
+
+    cfg.pin = GPIO_Pin_8;
+    cfg.mode = Mode_Out_PP;
+    cfg.speed = Speed_50MHz;
+
+    gpioInit(GPIOB, &cfg);
+}
+
+
+// ----------------------------------------------------------------------------
+// ----------------------------------------------------------------------------
+
+void setup() {
+    printfSupportInit();
+
+    systemInit();
+
+    // initialize IO (needed for all IO operations)
+    IOInitGlobal();
+
+    initEEPROM();
+
+    ensureEEPROMContainsValidData();
+    readEEPROM();
+
+    ledInit(statusLedConfig());
+    LED2_ON;
+
+    EXTIInit();
+
+    delay(100);
+
+    timerInit();  // timer must be initialized before any channel is allocated
+
+    serialInit(feature(FEATURE_SOFTSERIAL), SERIAL_PORT_NONE);
+
+    spiInit(SPIDEV_1);
+
+    // Custom serial printer
+    cerealInit();
+
+    // led GPIOB_Pin_8
+    LEDInit();
+
+}
+
+// ----------------------------------------------------------------------------
+// ----------------------------------------------------------------------------
+
+void loop() {
+    // print
+    cerealProcess(5);
+
+    digitalHi(GPIOB, GPIO_Pin_8);   // turn the LED on (HIGH is the voltage level)
+    delay(500);               // wait for a second
+    digitalLo(GPIOB, GPIO_Pin_8);    // turn the LED off by making the voltage LOW
+    delay(500);                // wait for a second
+}
+
+// ----------------------------------------------------------------------------
+// ----------------------------------------------------------------------------
+
+int main(void) {
+    setup();
     while(1)
-        scheduler();
+        loop();
 }
